c851c1dee2e0783a2fe9bdb97105e4b02fd9ffb0
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-04 21:49:40 -0300

    feat(infra-sql): add dapper queue repository with atomic claim

---
+++ Worker.Infrastructure/Persistence/Sql/Repositories/SqlRecordQueueRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker.Infrastructure.Persistence.Sql.Repositories
+{
+    internal class SqlRecordQueueRepository
+    {
+    }
+}
