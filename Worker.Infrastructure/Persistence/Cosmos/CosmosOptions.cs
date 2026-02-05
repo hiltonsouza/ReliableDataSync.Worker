a7e60d4984db805c5ac2717ce7dc87712be96ee9
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-04 22:19:11 -0300

    feat(infra-cosmos): add cosmos options and document model

---
+++ Worker.Infrastructure/Persistence/Cosmos/CosmosOptions.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker.Infrastructure.Persistence.Cosmos
+{
+    internal class CosmosOptions
+    {
+    }
+}
---
+++ Worker.Infrastructure/Persistence/Cosmos/Documents/SyncRecordDocument.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker.Infrastructure.Persistence.Cosmos.Documents
+{
+    internal class SyncRecordDocument
+    {
+    }
+}
