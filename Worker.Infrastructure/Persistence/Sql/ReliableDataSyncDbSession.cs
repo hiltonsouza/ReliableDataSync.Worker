ba645f0e182433e58bbe9a14fc96fe80d4f14091
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-04 21:47:16 -0300

    feat(infra-sql): add sql db session factory

---
+++ Worker.Infrastructure/Persistence/Sql/ReliableDataSyncDbSession.cs
@@ -0,0 +1,13 @@
+﻿using Azure.Core.Pipeline;
+using Worker.Application.Abstractions.Persistence;
+
+
+namespace Worker.Infrastructure.Persistence.Sql
+{
+    //public sealed class ReliableDataSyncDbSession : IRecordQueueRepository
+    //{
+    //    readonly ReliableDataSyncDbContext _db;
+    //    readonly RetryPolicy _retryPolicy;
+    //    readonly AttemptLimitPolicy _attemptLimitPolicy;
+    //}
+}
