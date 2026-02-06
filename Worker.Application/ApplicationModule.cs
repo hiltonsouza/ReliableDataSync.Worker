4246e1005791c25109f90b9e01935bed5218faec
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-05 21:37:24 -0300

    feat(app): add application module for DI wiring

---
+++ Worker.Application/ApplicationModule.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Worker.Application.Services;
+using Worker.Application.UseCases;
+
+namespace Worker.Application
+{
+    public static class ApplicationModule
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddSingleton<ExecutionClassifier>();
+            services.AddScoped<ProcessNextRecordUseCase>();
+            return services;
+        }
+    }
+}
