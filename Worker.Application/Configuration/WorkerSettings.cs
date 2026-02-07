e388d78c3365ce9ce9c2521e22e5ba883b3c23c7
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-07 19:06:42 -0300

    feat(config): add application configuration classes

---
+++ Worker.Application/Configuration/WorkerSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Worker.Application.Configuration;
+
+public class WorkerSettings
+{
+    public int BatchSize { get; set; } = 100;
+    public int IntervalSeconds { get; set; } = 30;
+    public int MaxRetries { get; set; } = 3;
+}
