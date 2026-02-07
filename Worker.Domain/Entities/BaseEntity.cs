bfd8dd22e367e998866acf63d058e58d46520c0f
Hilton P Souza <h_iltonsouza@outlook.com>
2026-02-07 19:06:31 -0300

    feat(domain): add BaseEntity

---
+++ Worker.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿namespace Worker.Domain.Entities;
+
+public abstract class BaseEntity
+{
+    public Guid Id { get; protected set; }
+    public DateTime CreatedAt { get; protected set; }
+    public DateTime? UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; }
+
+    protected BaseEntity()
+    {
+        Id = Guid.NewGuid();
+        CreatedAt = DateTime.UtcNow;
+        IsDeleted = false;
+    }
+}
